f2a78e7a6afc39a70176fa1ddea2d5d3cdda022a
dgozos <38340601+dgozos@users.noreply.github.com>
2021-04-13 23:14:13 -0700

    Page Routing and Sign In Page

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,234 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Center(child: Text('Gratitude Garden')),
-        ),
-        body: Center(child: Image(image: AssetImage('images/GG.jpg'))),
-      ),
-    ),//dsfklsdf
-  );
+      initialRoute: '/',
+      routes: {
+        '/': (context) => StartUp(),
+        '/sign_in': (context) => SignIn(),
+      },
+    ));
 }
+
+class StartUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Center(child: Text('Gratitude Garden')),
+      ),
+      body: Center(
+          child: Column(
+          children: <Widget>[
+            Image(image: AssetImage('images/GG.jpg')),
+            Text('Planting Gratitude with the Right Attitude', style: TextStyle(fontSize: 20.0)),
+            ElevatedButton(
+              child: Text('Sign In'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/sign_in');
+              }
+            ),
+            ElevatedButton(
+              child: Text('Create an Account'),
+              onPressed: () {},
+            )
+          ]
+      )),
+
+    );
+  }
+}
+
+//Image(image: AssetImage('images/GG.jpg'))
+
+  class SignIn extends StatefulWidget {
+    @override
+    _SignInState createState() => _SignInState();
+  }
+
+class _SignInState extends State<SignIn>{
+  @override
+  Widget build(BuildContext context) {
+    double width=MediaQuery.of(context).size.width;
+    double height=MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        height: height,
+        width: width,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              AppBar(
+                title: Center(child: Text('Gratitude Garden')),
+              ),
+              Container(
+                width: width,
+                height: height*0.25,
+                child: Image(image: AssetImage('images/GG.jpg'))
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text('Sign In', style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30.0,),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  suffixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                 ),
+                ),
+               ),
+              SizedBox(height: 30.0,),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  suffixIcon: Icon(Icons.visibility_off),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                 ),
+                ),
+              SizedBox(height: 30.0,),
+              Padding(
+                padding: const EdgeInsets.all (5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Forget Password?', style: TextStyle(fontSize: 12.0),),
+                    ElevatedButton(
+                      child: Text('Login'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => Second()));
+                },
+                child: Text.rich(
+                  TextSpan(
+                    text: 'Don\'t have an account? ',
+                    children: [
+                      TextSpan(
+                        text: 'Sign Up'
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Second extends StatefulWidget {
+  @override
+  _SecondState createState() => _SecondState();
+}
+
+class _SecondState extends State<Second> {
+  @override
+  Widget build(BuildContext context) {
+    double width=MediaQuery.of(context).size.width;
+    double height=MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        height: height,
+        width: width,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              AppBar(
+                title: Center(child: Text('Gratitude Garden')),
+              ),
+              Container(
+                  width: width,
+                  height: height*0.45,
+                  child: Image(image: AssetImage('images/GG.jpg'))
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text('Sign Up', style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30.0,),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  suffixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30.0,),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  suffixIcon: Icon(Icons.visibility_off),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30.0,),
+              Padding(
+                padding: const EdgeInsets.all (10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Forget Password?', style: TextStyle(fontSize: 12.0),),
+                    ElevatedButton(
+                      child: Text('Sign Up'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SignIn()));
+                },
+                child: Text.rich(
+                  TextSpan(
+                    text: 'Already have an account',
+                    children: [
+                      TextSpan(
+                          text: 'Sign In'
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
