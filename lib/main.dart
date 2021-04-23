d41c5226d38246832e2c07a1834f9754b4efdc76
dgozos <38340601+dgozos@users.noreply.github.com>
2021-04-22 21:07:09 -0700

    Friends List page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+//import 'package:search_widget/search_widget.dart';
 
 void main() {
   runApp(
@@ -370,8 +371,75 @@ Widget GardenPage() {
 }
 Widget FriendsPage() {
   // Builder widgets
-  return Container(
-    child: Text('Friends Page'),
+  return Scaffold(
+    backgroundColor: Colors.white,
+    appBar: AppBar(
+      backgroundColor: Colors.white,
+      title: Text('Friends List', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.blue),),
+    ),
+    body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('     Online', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.blue),),
+            Row(
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(image: AssetImage('images/GwenStacy.jpg'), fit: BoxFit.cover,),
+                  ),
+                ),
+                SizedBox(width: 10,),
+                Text('Gwen Stacy', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.blue),),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(image: AssetImage('images/HarryOsborn.jpg'), fit: BoxFit.cover,),
+                  ),
+                ),
+                SizedBox(width: 10,),
+                Text('Harry Osborn', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.blue),),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(image: AssetImage('images/MaryJane.jpg'), fit: BoxFit.cover,),
+                  ),
+                ),
+                SizedBox(width: 10,),
+                Text('Mary Jane', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.blue),),
+              ],
+            ),
+            Text('     Offline', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.blue),),
+            Row(
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(image: AssetImage('images/TonyStarks.jpg'), fit: BoxFit.cover,),
+                  ),
+                ),
+                SizedBox(width: 10,),
+                Text('Tony Starks', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.blue),),
+              ],
+            ),
+      ],
+    ),
   );
 }
 
